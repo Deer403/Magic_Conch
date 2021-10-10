7bfe9a06f1b3b93c6f9b06e6e9664f39f0d3bc60
deer404 <919187569@qq.com>
2021-10-10 15:48:40 +0800

    输出区域样式完成

--- conch-page/src/page/codepage/components/CodeContent.vue
+++ conch-page/src/page/codepage/components/CodeContent.vue
@@ -1,15 +1,32 @@
 <template>
-  <div class="content-wrapper">content</div>
+  <div class="content-wrapper">
+    <div v-for="(msg, index) in msgs" :key="index" class="msg-item">
+      <span>console>></span>
+      <span>{{msg}}</span>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   name: "CodeContent",
+  data: () => {
+    return {
+      msgs: ["正在连接服务器", "已连接上服务器...", "连接被意外关闭..."],
+    };
+  },
 };
 </script>
 
 <style>
 .content-wrapper {
-    padding-left: 30px;
+  padding-top: 10px;
+  padding-left: 20px;
+  font-family: Monospace !important;
+  font-size: 16px;
+}
+
+.msg-item{
+    margin: 5px 0px;
 }
 </style>
